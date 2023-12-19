3b59b5e6fede85a05bb4c919a4cab03958caa69b
itsaln <itsaln23@gmail.com>
2023-12-19 14:05:34 +0500

    01

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from '@/app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule, { cors: true })
+
+	// const config = new DocumentBuilder()
+	// 	.setTitle('Itsaln API')
+	// 	.setDescription('The api of my portfolio project')
+	// 	.setVersion('1.0')
+	// 	.build()
+
+	// const document = SwaggerModule.createDocument(app, config)
+	// SwaggerModule.setup('api', app, document)
+
+	app.setGlobalPrefix('api')
+	await app.listen(process.env.PORT || 5200)
+	console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+
+let ignore = bootstrap()
