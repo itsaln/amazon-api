e39d6fdcfa2d92ba7423af3e7ed30dc37262774c
itsaln <itsaln23@gmail.com>
2023-12-22 19:12:04 +0500

    statistics

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { StatisticsService } from '@app/statistics/statistics.service'
+import { Auth } from '@app/auth/decorators/auth.decorator'
+import { CurrentUser } from '@app/auth/decorators/user.decorator'
 
 @Controller('statistics')
 export class StatisticsController {
 	constructor(private readonly statisticsService: StatisticsService) {}
+
+	@Auth()
+	@Get('main')
+	getMainStatistics(@CurrentUser('id') id: number) {
+		return this.statisticsService.getMain(id)
+	}
 }
--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@app/prisma.service'
+import { UserService } from '@app/user/user.service'
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+	constructor(
+		private prismaService: PrismaService,
+		private userService: UserService
+	) {}
+
+	async getMain(userId: number) {
+		const user = await this.userService.byId(userId, {
+			orders: {
+				select: {
+					items: true
+				}
+			},
+			reviews: true
+		})
+
+		// TODO: Закончить статистику
+
+		// // PLEASE DON'T USE IN PRODUCTION
+		// for (let order of user.orders) {
+		// 	let total= 0
+		//
+		// 	for (let price of order.price) {
+		// 		total += price.value
+		// 	}
+		// 	;(order as any).total = total
+		// }
+
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},
+			{
+				name: 'Reviews',
+				value: user.reviews.length
+			},
+			{
+				name: 'Favorites',
+				value: user.favorites.length
+			},
+			{
+				name: 'Total amount',
+				value: 1000
+			}
+		]
+	}
+}
