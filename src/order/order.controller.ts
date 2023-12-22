72a195b03dbc8594651499e0d716f1ea2f18c810
itsaln <itsaln23@gmail.com>
2023-12-22 23:50:56 +0500

    order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { OrderService } from '@app/order/order.service'
+import { Auth } from '@app/auth/decorators/auth.decorator'
+import { CurrentUser } from '@app/auth/decorators/user.decorator'
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
 	constructor(private readonly orderService: OrderService) {}
+
+	@Auth()
+	@Get()
+	getAll(@CurrentUser('id') userId: number) {
+		return this.orderService.getAll(userId)
+	}
 }
